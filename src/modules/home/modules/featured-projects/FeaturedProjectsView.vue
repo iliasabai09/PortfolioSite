8e8435dfea89ece8792d441bb3d1d3f3da6bb8c1
iliasabai09 <iliasabaimain@gmail.com>
2025-12-24 20:39:51 +0500

    feat:(#1) fix design

--- src/modules/home/modules/featured-projects/FeaturedProjectsView.vue
+++ src/modules/home/modules/featured-projects/FeaturedProjectsView.vue
@@ -32,7 +32,8 @@ import {PROJECTS} from "./constants";
   }
 
   &-cards {
-    display: flex;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
     justify-content: center;
     gap: 24px;
     flex-wrap: wrap;
